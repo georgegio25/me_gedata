db36ea39e92b117c57bb213578b898f5b67f9f37
George Gr <97710311+georgegio25@users.noreply.github.com>
2024-03-07 23:26:51 -0800

    Update users.py
    controller for users of the website

--- users.py
+++ users.py
@@ -15,3 +15,18 @@ def create_user():
     if User.registry_validation(request.form):  
         session["user_id"] = User.create(request.form) 
     return redirect("/")
+
+@app.route("/login", methods = ["POST"]) 
+def login():
+    if not User.login_validator(request.form): 
+        flash("Invalid login") 
+        return redirect("/")
+
+    user = User.get_by_email(request.form) 
+    session["user_id"] = user.id  
+    return redirect("/main")
+
+@app.route("/logout")    
+def logout():
+    session.clear()     
+    return redirect("/")
